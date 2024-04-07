976bf5e88fae6661559130d1f41599515c608944
Ansel Hartanto <anselhartanto@gmail.com>
2024-04-06 22:30:50 -0400

    add base Volunteer model

--- townhall/myapi/models.py
+++ townhall/myapi/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Volunteer(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    age = models.IntegerField()
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+
+    def __str__(self):
+        return self.first_name
