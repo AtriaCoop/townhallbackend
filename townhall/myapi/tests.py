170c298867d1d500698e230cb136591d3dd21fef
GagenvirGill <gagenvirg@gmail.com>
2024-07-15 18:59:08 -0700

    Changed the service method get volunteer info’s tests to include the email field

--- townhall/myapi/tests.py
+++ townhall/myapi/tests.py
@@ -6,14 +6,15 @@ from myapi import services as townhall_services
 
 class TownhallTestCase(TestCase):
     def setUp(self):
-        townhall_models.Volunteer.objects.create(id=1, first_name="Zamorak", last_name="Red", age=11)
-        townhall_models.Volunteer.objects.create(id=2, first_name="Guthix", last_name="Green", age=77)
+        townhall_models.Volunteer.objects.create(id=1, first_name="Zamorak", last_name="Red", age=11, email="zamorak.red@gmail.com")
+        townhall_models.Volunteer.objects.create(id=2, first_name="Guthix", last_name="Green", age=77, email="guthix_green@hotmail.ca")
 
     def test_get_volunteer(self):
         volunteer_1 = townhall_services.VolunteerServices.get_volunteer(id=1)
         assert volunteer_1.first_name == "Zamorak"
         assert volunteer_1.last_name == "Red"
         assert volunteer_1.age == 11
+        assert volunteer_1.email == "zamorak.red@gmail.com"
 
     def test_dummy_test(self):
         pass
