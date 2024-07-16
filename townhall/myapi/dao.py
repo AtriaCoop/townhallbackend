fa2d215b56efa041d64b02f6c5232c491859d76b
Ryan <ryeean16@gmail.com>
2024-07-15 19:03:02 -0700

    added opportunity dao to create opportunity

--- townhall/myapi/dao.py
+++ townhall/myapi/dao.py
@@ -1,5 +1,7 @@
 from .models import Volunteer
+from .models import Opportunity
 from .types import CreateVolunteerData
+from .types import CreateOpportunityData
 # Follows layered architecture pattern of views -> services -> dao
 
 class VolunteerDao:
@@ -13,9 +15,18 @@ class VolunteerDao:
     
     def create_volunteer(create_volunteer_data: CreateVolunteerData):
         Volunteer.objects.create(
-            first_name=create_volunteer_data.first_name,
-            last_name=create_volunteer_data.last_name,
+            name=create_volunteer_data.name,
             gender=create_volunteer_data.gender,
             age=create_volunteer_data.age,
             email=create_volunteer_data.email,
+        )
+
+class OpportunityDao:
+
+    def create_opportunity(create_opportunity_data: CreateOpportunityData):
+        Opportunity.objects.create(
+            name=create_opportunity_data.name,
+            time=create_opportunity_data.time,
+            description=create_opportunity_data.description,
+            location=create_opportunity_data.location
         )
