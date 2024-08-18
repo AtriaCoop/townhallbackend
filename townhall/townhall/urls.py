9079f4282685ffccacb7b436bb847fcade67ae8b
JordanBandur <jordanbandur@hotmail.ca>
2024-08-18 14:06:22 -0600

    Update to include PUT endpoint for volunteer

--- townhall/townhall/urls.py
+++ townhall/townhall/urls.py
@@ -23,5 +23,6 @@ from myapi.views import OpportunityViewSet
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('volunteer/', VolunteerViewSet.as_view({'get': 'handle_volunteer_request'})),
+    path('volunteer/<int:pk>/update/', VolunteerViewSet.as_view({'put': 'update_volunteer'}), name='update_volunteer'),
     path('opportunity/', OpportunityViewSet.as_view({'get': 'handle_opportunity_request', 'delete': 'handle_opportunity_delete'})),
 ]
