ca6dcc1af494f919a59b7c7d35b08e332bbb4c78
Ryan <ryeean16@gmail.com>
2024-08-26 17:28:25 -0700

    1:M relationship between Post and Comment

--- townhall/myapi/admin.py
+++ townhall/myapi/admin.py
@@ -4,10 +4,20 @@ from django.contrib import admin
 from .models import Volunteer
 from .models import Organization
 from .models import Opportunity
-from .models import Post
+from .models import Post, Comment
+
+# Displays the comments under the post in tabular form (neat table).
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1  # Number of extra blank comment fields
+
+# When viewing a post, you can see the associated comments
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
 
 
 admin.site.register(Volunteer)
 admin.site.register(Opportunity)
 admin.site.register(Organization)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
