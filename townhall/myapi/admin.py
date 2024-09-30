07dcc3d8dcf00a5afc1c612fd83aa093bc3c33ba
JordanBandur <jordanbandur@hotmail.ca>
2024-09-29 23:52:56 -0600

    Add Task related admin

--- townhall/myapi/admin.py
+++ townhall/myapi/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Volunteer
 from .models import Organization
 from .models import Opportunity
-from .models import Post, Comment
+from .models import Post, Comment, Task
 
 
 # Displays the comments under the post in tabular form (neat table).
@@ -17,6 +17,12 @@ class CommentInline(admin.TabularInline):
 class PostAdmin(admin.ModelAdmin):
     inlines = [CommentInline]
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'status', 'deadline', 'assigned_to', 'created_by']
+    search_fields = ['name', 'description']
+    list_filter = ['status', 'deadline', 'assigned_to']
 
 admin.site.register(Volunteer)
 admin.site.register(Opportunity)
