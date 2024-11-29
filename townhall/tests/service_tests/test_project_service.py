0e7bf3426940048002a28af6455393ba812985ee
steven huang <114288160+stevenh17@users.noreply.github.com>
2024-11-28 23:16:11 -0800

    use self.asserts instead of default assert

--- townhall/tests/service_tests/test_project_service.py
+++ townhall/tests/service_tests/test_project_service.py
@@ -4,11 +4,6 @@ from myapi import services as townhall_services
 from datetime import datetime
 from django.utils import timezone
 
-# Project
-
-# Testing ALL tests "python manage.py test tests"
-# Testing ONLY project "python manage.py test tests.service_tests.test_project_service"
-
 
 class TestProjectModel(TestCase):
     def setUp(self):
@@ -18,7 +13,7 @@ class TestProjectModel(TestCase):
             description="Description1",
             start_date=timezone.make_aware(datetime(2024, 7, 20, 10, 0)),
             end_date=timezone.make_aware(datetime(2024, 7, 20, 20, 30)),
-        ),
+        )
         townhall_models.Project.objects.create(
             id=2,
             title="Project2",
@@ -29,22 +24,27 @@ class TestProjectModel(TestCase):
 
     def test_get_project(self):
         project = townhall_services.ProjectServices.get_project(id=1)
-        assert project.title == "Project1"
-        assert project.description == "Description1"
-        assert project.start_date == timezone.make_aware(datetime(2024, 7, 20, 10, 0))
-        assert project.end_date == timezone.make_aware(datetime(2024, 7, 20, 20, 30))
+        self.assertIsNotNone(project)
+        self.assertEqual(project.title, "Project1")
+        self.assertEqual(project.description, "Description1")
+        self.assertEqual(
+            project.start_date, timezone.make_aware(datetime(2024, 7, 20, 10, 0))
+        )
+        self.assertEqual(
+            project.end_date, timezone.make_aware(datetime(2024, 7, 20, 20, 30))
+        )
 
     def test_get_project_not_found(self):
         project = townhall_services.ProjectServices.get_project(id=100)
-        assert project is None
+        self.assertIsNone(project)
 
     def test_get_all_projects(self):
         projects = townhall_services.ProjectServices.get_all_projects()
-        assert len(projects) == 2
-        assert projects[0].title == "Project1"
-        assert projects[1].title == "Project2"
+        self.assertEqual(len(projects), 2)
+        self.assertEqual(projects[0].title, "Project1")
+        self.assertEqual(projects[1].title, "Project2")
 
     def test_get_all_projects_empty(self):
         townhall_models.Project.objects.all().delete()
         projects = townhall_services.ProjectServices.get_all_projects()
-        assert len(projects) == 0
+        self.assertEqual(len(projects), 0)
